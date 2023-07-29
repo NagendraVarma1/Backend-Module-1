2a7dfc84c22fe797e903d82e877eeb107497ab1d
NagendraVarma1 <nagendravarmal512>
2023-07-29 11:56:06 +0530

    Object

--- task2.js
+++ task2.js
@@ -1,9 +1,20 @@
-const firstNum = 2;
-let secondNum = 4;
+// const firstNum = 2;
+// let secondNum = 4;
 
 
-const fun = (a, b) => {
-    console.log(a * b);
+// const fun = (a, b) => {
+//     console.log(a * b);
+// }
+
+// fun(firstNum,secondNum);
+
+const student = {
+    name: 'Varma',
+    age: 24,
+    university: 'Centurion University',
+    details: function() {
+        console.log('Hi, this is ' + this.name+', completed my graduation in '+ this.university)
+    }
 }
+student.details();
 
-fun(firstNum,secondNum)
