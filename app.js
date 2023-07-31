24a1fc2d7d06947cdfa5fa4ff3c15c7c4d78ed92
NagendraVarma1 <nagendravarmal512>
2023-07-31 10:10:13 +0530

    Task7

--- app.js
+++ app.js
@@ -1,8 +1,30 @@
-const http = require('http');
+const http = require("http");
 
 const server = http.createServer((req, res) => {
-    console.log('Varma');
-})
+  if (req.url === "/home") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>My First Page</title></head>");
+    res.write("<body><h1>Welcome Home</h1></body>");
+    res.write("</html>");
+    res.end();
+  }
+  if (req.url === "/about") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>My First Page</title></head>");
+    res.write("<body><h1>Welcome to About Us Page</h1></body>");
+    res.write("</html>");
+    res.end();
+  }
+  if (req.url === "/node") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>My First Page</title></head>");
+    res.write("<body><h1>Welcome to my Node Js project</h1></body>");
+    res.write("</html>");
+    res.end();
+  }
+});
 
-server.listen(4000)
-
+server.listen(3000);
