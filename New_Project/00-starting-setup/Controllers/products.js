c31dd0a96d0345794b098f9755a4ad9c3ddae144
NagendraVarma1 <nagendravarmal512>
2023-08-21 17:35:26 +0530

    Task 7

--- New_Project/00-starting-setup/Controllers/products.js
+++ New_Project/00-starting-setup/Controllers/products.js
@@ -1,5 +1,4 @@
-
-const products = [];
+const Product = require("../models/product");
 
 exports.getAddProduct = (req, res, next) => {
   res.render("add-product", {
@@ -12,17 +11,20 @@ exports.getAddProduct = (req, res, next) => {
 };
 
 exports.postAddProduct = (req, res, next) => {
-    products.push({ title: req.body.title });
-    res.redirect('/');
-  }
+  const product = new Product(req.body.title);
+  product.save();
+  res.redirect("/");
+};
 
-  exports.getProducts = (req, res, next) => {
-    res.render('shop', {
+exports.getProducts = (req, res, next) => {
+  const products = Product.fetchAll((products) => {
+    res.render("shop", {
       prods: products,
-      pageTitle: 'Shop',
-      path: '/',
+      pageTitle: "Shop",
+      path: "/",
       hasProducts: products.length > 0,
       activeShop: true,
-      productCSS: true
+      productCSS: true,
     });
-  }
+  });
+};
