2721fd2d8f36e592d51f418441d9506ba7ee6395
NagendraVarma1 <nagendravarmal512>
2023-09-14 10:28:34 +0530

    Task 17

--- Project-2/00-starting-setup/util/database.js
+++ Project-2/00-starting-setup/util/database.js
@@ -1,10 +1,8 @@
-const mysql = require('mysql2');
+const Sequelize = require("sequelize");
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'node-complete',
-    password: 'NagendraVarma@512'
+const sequelize = new Sequelize("node-complete", "root", "NagendraVarma@512", {
+  dialect: "mysql",
+  host: "localhost",
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
